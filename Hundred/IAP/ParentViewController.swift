0fbfe0863fb7e084490d9bb3216595694e7f293a
igibliss00@gmail.com <igibliss00@gmail.com>
2020-10-15 03:03:58 -0400

    Add Terms of Use, Privacy Policy. Update ParentVC for IAP

--- Hundred/IAP/ParentViewController.swift
+++ Hundred/IAP/ParentViewController.swift
@@ -11,25 +11,56 @@ import UIKit
 
 class ParentViewController: UIViewController {
     // MARK: - Types
-    
     fileprivate enum Segment: Int {
         case products, purchases
     }
     
     // MARK: - Properties
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var restoreButton: UIBarButtonItem!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    /// contains the privacy button
+    fileprivate let infoContainerView = UIView()
+    fileprivate let privacyButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Privacy Policy", for: .normal)
+        button.titleLabel?.font = UIFont.caption
+        button.setTitleColor(.black, for: .normal)
+        button.tag = 1
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+    fileprivate let termsButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Terms of Use", for: .normal)
+        button.titleLabel?.font = UIFont.caption
+        button.setTitleColor(.black, for: .normal)
+        button.tag = 2
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+    fileprivate let infoBox: UIView = {
+        let infoBox = UIView()
+        return infoBox
+    }()
+    fileprivate let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Fine Print\n\nThe subscription allows you to post your entries on Public Feed so that you can share your progress with likeminded achievers. You can modify or delete your entries at any time.  Once you subscribe to one of the plans above, it will renew within 24 hours before the subscription period ends and you will be charged through your iTunes account.  You can manage your subscription in Account Settings and you can cancel your subscription at any time you want."
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = .gray
+        label.adjustsFontSizeToFitWidth = false
+        return label
+    }()
     
     fileprivate var utility = Utilities()
-    
     fileprivate lazy var products: Products = {
         let identifier = ViewControllerIdentifiers.products
         guard let controller = storyboard?.instantiateViewController(withIdentifier: identifier) as? Products
             else { fatalError("\(Messages.unableToInstantiateProducts)") }
         return controller
     }()
-    
     fileprivate lazy var purchases: Purchases = {
         let identifier = ViewControllerIdentifiers.purchases
         guard let controller = storyboard?.instantiateViewController(withIdentifier: identifier) as? Purchases
@@ -42,20 +73,89 @@ class ParentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureUI()
+        
         // Disable or hide the button.
         restoreButton.disable()
         
         StoreManager.shared.delegate = self
         StoreObserver.shared.delegate = self
         
-        // Fetch product information.
+        // Fetch product information from App Store.
         fetchProductInformation()
         
         if #available(iOS 13.0, *) {
             // Always adopt a light interface style.
             overrideUserInterfaceStyle = .light
         }
+    }
+    
+    // MARK: - Configure UI
+    
+    func configureUI() {
+        scrollView.contentSize = CGSize(width: view.frame.width, height: 950)
+        
+        scrollView.addSubview(infoBox)
+        BorderStyle.customShadowBorder(for: infoBox)
+        infoBox.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            infoBox.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 550),
+            infoBox.heightAnchor.constraint(equalToConstant: 300),
+            infoBox.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.85),
+            infoBox.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
+        
+        infoBox.addSubview(infoLabel)
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            infoLabel.widthAnchor.constraint(equalTo: infoBox.widthAnchor, multiplier: 0.90),
+            infoLabel.heightAnchor.constraint(equalTo: infoBox.heightAnchor, multiplier: 0.90),
+            infoLabel.centerXAnchor.constraint(equalTo: infoBox.centerXAnchor),
+            infoLabel.centerYAnchor.constraint(equalTo: infoBox.centerYAnchor)
+        ])
+        
+        scrollView.addSubview(infoContainerView)
+        infoContainerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            infoContainerView.heightAnchor.constraint(equalToConstant: 30),
+            infoContainerView.topAnchor.constraint(equalTo: infoBox.bottomAnchor, constant: 20),
+            infoContainerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.90),
+            infoContainerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
 
+        infoContainerView.addSubview(privacyButton)
+        privacyButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            privacyButton.widthAnchor.constraint(equalToConstant: 150),
+            privacyButton.heightAnchor.constraint(equalToConstant: 30),
+            privacyButton.centerYAnchor.constraint(equalTo: infoContainerView.centerYAnchor)
+        ])
+        
+        infoContainerView.addSubview(termsButton)
+        termsButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            termsButton.widthAnchor.constraint(equalToConstant: 150),
+            termsButton.heightAnchor.constraint(equalToConstant: 30),
+            termsButton.centerYAnchor.constraint(equalTo: infoContainerView.centerYAnchor),
+            termsButton.trailingAnchor.constraint(equalTo: infoContainerView.trailingAnchor)
+        ])
+    }
+    
+    // MARK: Button Pressed for links
+    
+    @objc func buttonPressed(sender: UIButton!) {
+        switch sender.tag {
+        case 1:
+            if let url = URL(string: "https://sites.google.com/view/thehundredapp/home") {
+                UIApplication.shared.open(url)
+            }
+        case 2:
+            if let url = URL(string: "https://sites.google.com/view/thehundredappterms/home") {
+                UIApplication.shared.open(url)
+            }
+        default:
+            print("default")
+        }
     }
     
     // MARK: - Switching Between View Controllers
@@ -64,13 +164,15 @@ class ParentViewController: UIViewController {
     fileprivate func addBaseViewController(_ viewController: IAPViewController) {
         addChild(viewController)
         viewController.view.translatesAutoresizingMaskIntoConstraints = false
-        viewController.view.frame = containerView.bounds
-        containerView.addSubview(viewController.view)
+        viewController.view.frame = scrollView.bounds
+        scrollView.addSubview(viewController.view)
         
-        NSLayoutConstraint.activate([viewController.view.topAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.topAnchor),
-                                     viewController.view.bottomAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.bottomAnchor),
-                                     viewController.view.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
-                                     viewController.view.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor)])
+        NSLayoutConstraint.activate([
+            viewController.view.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            viewController.view.heightAnchor.constraint(equalToConstant: 500),
+            viewController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.95),
+            viewController.view.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
         viewController.didMove(toParent: self)
     }
     
